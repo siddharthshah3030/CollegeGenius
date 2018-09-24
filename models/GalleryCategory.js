b64da09300b066aa5d91830d139f45b0cea54347
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-09-24 11:47:40 +0530

    ctegry gallery bug removed

--- models/GalleryCategory.js
+++ models/GalleryCategory.js
@@ -1,16 +1,16 @@
-var keystone = require('keystone');
-var Types = keystone.Field.Types;
+// var keystone = require('keystone');
+// var Types = keystone.Field.Types;
 
-var PostCategory = new keystone.List('PostCategory', {
-	autokey: { from: 'name', path: 'key', unique: true },
-	label: 'Categories',
-});
+// var PostCategory = new keystone.List('PostCategory', {
+// 	autokey: { from: 'name', path: 'key', unique: true },
+// 	label: 'Categories',
+// });
 
-PostCategory.add({
-	name: { type: String, required: true },
-});
+// PostCategory.add({
+// 	name: { type: String, required: true },
+// });
 
-PostCategory.relationship({ ref: 'Gallery', refPath: 'categories' });
+// PostCategory.relationship({ ref: 'Gallery', refPath: 'categories' });
 
-PostCategory.track = true;
-PostCategory.register();
+// PostCategory.track = true;
+// PostCategory.register();
