9a03de16d53c1ddb89852e038083f5c6a209d895
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-10-05 02:33:59 +0530

    doubt.js adding

---
+++ models/doubts.js
@@ -0,0 +1,30 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+var Doubts = new keystone.List('Doubts', {
+	autokey: { from: 'name', path: 'key', unique: true },
+});
+
+Doubts.add({
+	name: { type: String, required: true },
+	state: { type: Types.Select, options: 'unsolved, solved', default: 'unsolved', index: true },
+	questioner: { type: Types.Relationship, ref: 'User', index: true },
+	publishedDate: { type: Types.Date, index: true },
+	image: { type: Types.CloudinaryImage },
+	content: {
+		brief: { type: Types.Html, wysiwyg: true, height: 150 },
+		extended: { type: Types.Html, wysiwyg: true, height: 400 },
+	},
+	// categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
+});
+
+Doubts.schema.virtual('content.full').get(function () {
+	return this.content.extended || this.content.brief;
+});
+
+// Doubts.relationship({ path: 'comments', ref: 'PostComment', refPath: 'post' });
+
+Doubts.track = true;
+Doubts.defaultColumns = 'name, state|20%, author|20%, publishedDate|20%';
+Doubts.register();
+
