40aa92b01dd7159d811479843fa50c6324f67373
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-10-05 18:22:21 +0530

    basic quiz detail shown

--- public/js/quiz/quiz_detail.js
+++ public/js/quiz/quiz_detail.js
@@ -1,10 +1,23 @@
-let quizes = document.querySelector("#quiz")
+let questions = document.querySelector("#questions")
+let title = document.querySelector("#title")
 
 fetch('/quiz/list/').then(d => d.json()).then(d => {
-    q = q.filter((item,i) => i==location.href.split('/').pop())
+    d = d.filter((item,i) => i==location.href.split('/').pop())[0]
+    title.innerHTML = d.name
     fill_questions(d)
 })
 
 fill_questions= (d) => {
-
+    console.log(JSON.stringify(d,null,2))
+    d.questions.forEach((q,i)=> {
+        questions.innerHTML+=`
+            <h3>${i+1}. ${q.name}</h3>
+            <div class=options>
+                <input type=radio name=${i}>a)<span> ${q.c1}</span><br>
+                <input type=radio name=${i}>b)<span> ${q.c2}</span><br>
+                <input type=radio name=${i}>c)<span> ${q.c3}</span><br>
+                <input type=radio name=${i}>d)<span> ${q.c4}</span><br>
+            </div>
+        `
+    })
 }
