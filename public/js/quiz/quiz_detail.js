ec9ba61abd12a25fbb81a74f6318a0f2fca9c021
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-10-05 18:58:09 +0530

    quiz works all good

--- public/js/quiz/quiz_detail.js
+++ public/js/quiz/quiz_detail.js
@@ -1,10 +1,15 @@
+let quiz_div = document.querySelector("#quiz")
 let questions = document.querySelector("#questions")
 let title = document.querySelector("#title")
+let submit_btn = document.querySelector('#submit')
+let quiz
+let result=``
 
 fetch('/quiz/list/').then(d => d.json()).then(d => {
     d = d.filter((item,i) => i==location.href.split('/').pop())[0]
     title.innerHTML = d.name
     fill_questions(d)
+    quiz=d
 })
 
 fill_questions= (d) => {
@@ -13,11 +18,59 @@ fill_questions= (d) => {
         questions.innerHTML+=`
             <h3>${i+1}. ${q.name}</h3>
             <div class=options>
-                <input type=radio name=${i}>a)<span> ${q.c1}</span><br>
-                <input type=radio name=${i}>b)<span> ${q.c2}</span><br>
-                <input type=radio name=${i}>c)<span> ${q.c3}</span><br>
-                <input type=radio name=${i}>d)<span> ${q.c4}</span><br>
+                <input type=radio name=${i} class=c${i} value=${q.c1}>a)<span> ${q.c1}</span><br>
+                <input type=radio name=${i} class=c${i} value=${q.c2}>b)<span> ${q.c2}</span><br>
+                <input type=radio name=${i} class=c${i} value=${q.c3}>c)<span> ${q.c3}</span><br>
+                <input type=radio name=${i} class=c${i} value=${q.c4}>d)<span> ${q.c4}</span><br>
             </div>
         `
     })
+}
+
+// submit action
+submit_btn.addEventListener('click', e=>submit_action(e))
+
+submit_action = (e) => {
+    e.preventDefault()
+    console.log('quiz ended')
+
+    const len=quiz.questions.length
+    correct = 0
+
+
+    // calculate the correct questions
+    for(let i=0;i<len;i++){
+        var question = document.querySelectorAll(`.c${i}`)
+        question.forEach(item => {
+            if(item.checked){
+                console.log('triggered')
+                if(item.value==quiz.questions[i].ans){
+                    correct += 1
+                    result+=`
+                    <div class='row'>
+                        <div class='col-sm-1 bg-default'>${i+1}</div>
+                        <div class='col-sm-5 bg-success'>${item.value}</div>
+                        <div class='col-sm-5 bg-primary'>${quiz.questions[i].ans}</div>
+                    </div>
+                    `
+                }else {
+                    result+=`
+                    <div class='row'>
+                        <div class='col-sm-1 bg-default'>${i+1}</div>
+                        <div class='col-sm-5 bg-danger'>${item.value}</div>
+                        <div class='col-sm-5 bg-primary'>${quiz.questions[i].ans}</div>
+                    </div>
+                    `
+                }
+                console.log(item.value,quiz.questions[i].ans)
+            }
+        })
+    }
+
+    result+=`
+        <h1 class='my-5 display-4 text-center bg-default'>
+            score = ${correct}/${len}
+        </h1>
+    `
+    quiz_div.innerHTML = result
 }
