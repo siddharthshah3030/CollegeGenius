da0d8fe87f7c40e9f3d36289e1f6f4e2236cb117
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-10-06 01:42:27 +0530

    previous year clone of gallery wokring

--- models/previous.js
+++ models/previous.js
@@ -1,18 +1,28 @@
 var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
-var previous = new keystone.List('previous', {
+var Previous = new keystone.List('Previous', {
+	nodelete: false,
+
 	autokey: { from: 'name', path: 'key', unique: true },
-	plural: 'previous year question papers',
-	singular: 'previous year question papers',
+	plural: 'Previous Year Question Paper',
+	singular: 'Previous Year Questions Papers',
 });
 
-previous.add({
+Previous.add({
 	name: { type: String, required: true },
+	publishedDate: { type: Types.Date, default: Date.now },
+	images: { type: Types.CloudinaryImages, many:true },
 	Sem: { type: Types.Select, options: [
-		{ value: '3rd sem', label: "3rd sem" },
+		{ value: '1st sem', label: "1st sem" },
 		{ value: '2nd sem', label: "2nd sem" },
+		{ value: '3rd sem', label: "3rd sem" },
 		{ value: '4th sem', label: "4th sem" },
+		{ value: '5th sem', label: "5th sem" },
+		{ value: '6th sem', label: "6th sem" },
+		{ value: '7th sem', label: "7th sem" },
+		{ value: '8th sem', label: "8th sem" },
+
 	], required: false },
 	Branch: { type: Types.Select, options: [
 		{ value: 'ETC', label: "ETC" },
@@ -20,22 +30,38 @@ previous.add({
 		{ value: 'Mech', label: "Mech" },
 		{ value: 'IT', label: "IT" },
 		{ value: 'Archi', label: "Archi" },
-	], required: false },
-	Year: { type: Types.Select, options: [
-		{ value: '2015', label: "2015" },
-		{ value: '2016', label: "2016" },
-		{ value: '2017', label: "2017" },
-		{ value: '2018', label: "2018" },
-		{ value: '2019', label: "2019" },
-	], required: false },
 
-	publishedDate: { type: Types.Date, default: Date.now },
-	images: { type: Types.CloudinaryImages },
-	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
+		{ value: 'BioTech', label: "BioTech" },
+		{ value: 'BioMed', label: "BioMed" },
+		{ value: 'Chem', label: "Chem" },
+		{ value: 'Meta', label: "Meta" },
+		{ value: 'MCA', label: "MCA" },
+		{ value: 'Mining', label: "Mining" },
+		{ value: 'Electrical', label: "Electrical" },	], required: false },
+		// topics: { type: Types.Relationship, ref: 'previouscategory', many: true },
+
+
+		Year: { type: Types.Select, options: [
+			{ value: '2015', label: "2015" },
+			{ value: '2016', label: "2016" },
+			{ value: '2017', label: "2017" },
+			{ value: '2018', label: "2018" },
+			{ value: '2019', label: "2019" },
+			{ value: '2014', label: "2014" },
+			{ value: '2013', label: "2013" },
+			{ value: '2012', label: "2012" },
+			{ value: '2011', label: "2011" },
+			{ value: '2010', label: "2010" },			
+		], required: false },
+		Exam: { type: Types.Select, options: [
+			{ value: 'EndSem', label: "EndSem" },
+			{ value: 'CT1', label: "CT1" },
+			{ value: 'CT2', label: "CT2" },]
+		}
 
 });
 
-previous.track = true;
-previous.defaultSort = 'name';
-previous.defaultColumns = 'name, publishedDate';
-previous.register();
+Previous.track = true;
+Previous.defaultSort = 'name';
+Previous.defaultColumns = 'name,topics, publishedDate, Sem, Branch';
+Previous.register();
--- routes/views/previous.js
+++ routes/views/previous.js
@@ -1,15 +1,15 @@
 var keystone = require('keystone');
-var previous = keystone.list('previous');
+var Previous = keystone.list('Previous');
 
 exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
-	locals.section = 'previous';
+	locals.section = 'Previous Year Question Papers';
 
-	view.query('previous', previous.model.find().sort('sortOrder'));
-
+	view.query('previous', Previous.model.find().sort('sortOrder'));
+// console.log(Gallery.model.find().sort('sortOrder'))
 	view.render('previous');
 
 }
