a12ccf807745694991185dfe98b6972cf4386ae0
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-10-07 08:52:07 +0530

    front

--- routes/views/notification.js
+++ routes/views/notification.js
@@ -1,5 +1,6 @@
 var keystone = require('keystone');
 var Notification = keystone.list('Notification');
+var User = keystone.list('User');
 
 exports = module.exports = function (req, res) {
 
@@ -13,8 +14,34 @@ exports = module.exports = function (req, res) {
 		event.forEach(e => {
 			// console.log(e.notify)
 			if(e.notify===true){
-				var something = 		Notification.model.findByIdAndUpdate(e._id, {notify:false})
-				console.log(something)
+		var something = Notification.model.findByIdAndUpdate(e._id, {notify:false}, (de)=>{
+			User.model.find().exec(function(err, user) {
+				console.log(user)
+				var request = require('request');
+request("http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=9770306466&authkey=241506ApoDT2oa42n5bb94553&message="+e.name, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Print the google web page.
+  }
+})
+request("http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=7879563974&authkey=241506ApoDT2oa42n5bb94553&message=there is a new event "+e.name+ " please check it out  ", function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Print the google web page.
+  }
+})
+				// fetch("http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=9770306466&authkey=241506ApoDT2oa42n5bb94553&message="+e.name)
+				// .then(function(response) {
+				// //   return response.json();
+				// })
+				// .then(function(myJson) {
+				//   console.log(JSON.stringify(myJson));
+				// });
+				// $.get( "http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=9770306466&authkey=241506ApoDT2oa42n5bb94553&message="+e.name, function( data ) {
+				// 	// $( ".result" ).html( data );
+				// 	alert( "Load was performed." );
+				//   });
+			})
+					console.log(e);
+				})
 			}
 		});
 		// res.json(quiz)
