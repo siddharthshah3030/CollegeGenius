90fa65d730090b79b386a82c4864febd3aca5d2f
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-10-08 00:25:04 +0530

    disable sms

--- routes/views/notification.js
+++ routes/views/notification.js
@@ -18,32 +18,24 @@ exports = module.exports = function (req, res) {
 			User.model.find().exec(function(err, user) {
 				console.log(user)
 				var request = require('request');
-request("http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=9770306466&authkey=241506ApoDT2oa42n5bb94553&message=There is a new event named "+e.name+ " on the date " +e._.event_date.format('MMMM Do, YYYY')+ ", please check it out.", function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
-  }
-})
-request("http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=7879563974&authkey=241506ApoDT2oa42n5bb94553&message=There is a new event named "+e.name+ " on the date " +e._.event_date.format('MMMM Do, YYYY')+ ", please check it out.", function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
-  }
-})
-request("http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=9098847299&authkey=241506ApoDT2oa42n5bb94553&message=There is a new event named "+e.name+ " on the date " +e._.event_date.format('MMMM Do, YYYY')+ ", please check it out.", function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
-  }
-})
-				// fetch("http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=9770306466&authkey=241506ApoDT2oa42n5bb94553&message="+e.name)
-				// .then(function(response) {
-				// //   return response.json();
-				// })
-				// .then(function(myJson) {
-				//   console.log(JSON.stringify(myJson));
-				// });
-				// $.get( "http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=9770306466&authkey=241506ApoDT2oa42n5bb94553&message="+e.name, function( data ) {
-				// 	// $( ".result" ).html( data );
-				// 	alert( "Load was performed." );
-				//   });
+				//below is mobile sms code 
+				// don't enable unless really you want to 
+// request("http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=9770306466&authkey=241506ApoDT2oa42n5bb94553&message=There is a new event named "+e.name+ " on the date " +e._.event_date.format('MMMM Do, YYYY')+ ", please check it out.", function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Print the google web page.
+//   }
+// })
+// request("http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=7879563974&authkey=241506ApoDT2oa42n5bb94553&message=There is a new event named "+e.name+ " on the date " +e._.event_date.format('MMMM Do, YYYY')+ ", please check it out.", function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Print the google web page.
+//   }
+// })
+// request("http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles=9098847299&authkey=241506ApoDT2oa42n5bb94553&message=There is a new event named "+e.name+ " on the date " +e._.event_date.format('MMMM Do, YYYY')+ ", please check it out.", function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Print the google web page.
+//   }
+// })
+
 			})
 					console.log(e);
 				})
